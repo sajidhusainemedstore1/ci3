4e723936fe3e4e667583623c769741966929a08a
SAJIDALI7 <sajidalicharawala773@gmail.com>
2025-09-04 11:52:36 +0530

    modified:   application/controllers/Users.php 	modified:   application/models/User_model.php

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -7,13 +7,11 @@ class Users extends CI_Controller {
         parent::__construct();
     }
 
-    // READ: Show all users
     public function index() {
         $data['users'] = $this->user_model->get_all();
         $this->load->view('users/index', $data);
     }
 
-    // CREATE: Show form + save
     public function create() {
         if ($this->input->post()) {
             $data = [
@@ -26,7 +24,6 @@ class Users extends CI_Controller {
         $this->load->view('users/create');
     }
 
-    // UPDATE: Show edit form + update
     public function edit($id) {
         $data['user'] = $this->user_model->get_by_id($id);
         if ($this->input->post()) {
@@ -40,7 +37,6 @@ class Users extends CI_Controller {
         $this->load->view('users/edit', $data);
     }
 
-    // DELETE
     public function delete($id) {
         $this->user_model->delete($id);
         redirect('users');
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,27 +1,22 @@
 <?php
 class User_model extends CI_Model {
 
-    // Fetch all users
     public function get_all() {
         return $this->db->get('users')->result_array();
     }
 
-    // Insert new user
     public function insert($data) {
         return $this->db->insert('users', $data);
     }
 
-    // Get single user by ID
     public function get_by_id($id) {
         return $this->db->get_where('users', ['id' => $id])->row_array();
     }
 
-    // Update user
     public function update($id, $data) {
         return $this->db->where('id', $id)->update('users', $data);
     }
 
-    // Delete user
     public function delete($id) {
         return $this->db->delete('users', ['id' => $id]);
     }
